885f676eb24f309c561ca384e69a87e0d7ee1d76
nielsklazenga <niels.klazenga@rbg.vic.gov.au>
2018-04-01 13:00:51 +1000

    Fixed navbar in Javascript

--- assets/js/jquery.vicflora-static.js
+++ assets/js/jquery.vicflora-static.js
@@ -15,46 +15,5 @@ $(function() {
             });
         }
 
-        var navbar = $('#navbar>ul:eq(0)');
-
-        var dropdown = $('<li/>', {
-            class: "dropdown"
-        }).appendTo(navbar);
-
-        $('<a/>', {
-            href: "#",
-            class: "dropdown-toggle",
-            "data-toggle": "dropdown",
-            role: "button",
-            "aria-expanded": "false"
-        }).append('More <span class="caret"></span>').appendTo(dropdown);
-
-        var dropdownMenu = $('<ul/>', {
-            class: "dropdown-menu",
-            role: "menu"
-        }).appendTo(dropdown);
-
-        var length = $('#navbar>ul:eq(0)>li').length - 1;
-        var offset = 5;
-
-        console.log($(window).width());
-        if ($(window).width() <= 1200) {
-            offset = 3;
-        }
-        console.log(offset);
-
-        $('#navbar>ul:eq(0)>li:lt(' + length + '):gt('+ offset + ')')
-                .appendTo(dropdownMenu);
-
-        if (dropdownMenu.children('li.active').length > 0) {
-            dropdown.addClass('active');
-        }
     }
 });
-
-/*
- *             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">More <span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-
- */
